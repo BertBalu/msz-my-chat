a647375a57082cb138868645ecc5fb8ecad286e0
Bálint Bertalan <blintber@gmail.com>
2020-04-24 14:06:04 +0200

    Remove build from gitignore

---
+++ build/precache-manifest.45d71e1c8deaa7cad80835df726ccac9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5c6d8b49921307b75d14ee76f61e702",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bcb5692139d0d39e3c0d",
+    "url": "./static/css/main.1ff3ba34.chunk.css"
+  },
+  {
+    "revision": "f9f1c85ed2dfb059483d",
+    "url": "./static/js/2.db39bea0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.db39bea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcb5692139d0d39e3c0d",
+    "url": "./static/js/main.c2660bed.chunk.js"
+  },
+  {
+    "revision": "c999e1aafeacb3931724",
+    "url": "./static/js/runtime-main.9edb4eab.js"
+  }
+]);
